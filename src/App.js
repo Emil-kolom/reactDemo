670c1839654f84b83482ae63edf57038260168b1
emil <ruislan.kolomoychenko@gmail.com>
2022-06-24 13:46:59 +0300

    implement sorting algorithm

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Post from "./components/Post";
 import './style/app.css'
 import PostList from "./components/PostList";
 import PostCreator from "./components/PostCreator";
+import MySelect from "./components/UI/select/MySelect";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -21,9 +22,25 @@ function App() {
     setPosts(posts.filter(p=>p.id !== post.id))
   }
 
+  const [selectedSort, setSortAlg] = useState('');
+  const sortPost = (sort)=>{
+    setSortAlg(sort)
+    //т.к. напрямую изменять состояние нельзя
+    setPosts([...posts].sort((a,b)=> a[sort].localeCompare(b[sort])));
+  }
+
     return (
     <div className="App">
       <PostCreator create={createPost}/>
+      <MySelect
+        defaultValue={"Сортировка по"}
+        value={selectedSort}
+        onChange={sortPost}
+        options={[
+          {value: 'title', name: 'По названию'},
+          {value: 'body', name: 'По описанию'}
+        ]}
+      />
       {posts.length !== 0
           ?
           <PostList remove={removePost} posts={posts} title="Список постов 1"/>

0edae590ec3b140acca1f0bcefb53d79da1ffd47
emil <ruislan.kolomoychenko@gmail.com>
2022-06-25 10:28:48 +0300

    create a post filter

--- src/App.js
+++ src/App.js
@@ -1,54 +1,50 @@
-import React, {useState} from "react";
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
-import Post from "./components/Post";
+import React, {useMemo, useState} from "react";
 import './style/app.css'
 import PostList from "./components/PostList";
 import PostCreator from "./components/PostCreator";
 import MySelect from "./components/UI/select/MySelect";
+import MyInput from "./components/UI/Input/MyInput";
+import PostFilter from "./components/PostFilter";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {id: 1, title: 'Map', body: "Description"},
-    {id: 2, title: 'In object', body: "Description"},
-    {id: 3, title: 'post', body: "Description"}
-  ]);
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Map', body: "Description"},
+        {id: 2, title: 'In object', body: "Description"},
+        {id: 3, title: 'post', body: "Description"}
+    ]);
+    const [filter, setFilter] = useState({sort:'', query:''});
 
-  const createPost = (newPost) =>{
-    setPosts([...posts, newPost])
-  }
+    //useMemo - кеширует и отслеживает изменения каждого объекта из переданных в массиве 2 параметром
+    const sortedPosts = useMemo(()=>{
+        console.log('Отработала ф-ция')
+        if(filter.sort){
+            return [...posts].sort((a,b)=> a[filter.sort].localeCompare(b[filter.sort]));
+        }
+        return posts;
+    }, [filter.sort, posts]);
 
-  function removePost(post){
-    setPosts(posts.filter(p=>p.id !== post.id))
-  }
+    const sortedAndSearchPosts = useMemo(()=>{
+        return sortedPosts.filter( post => post.title.toLowerCase().includes(filter.query))
+    }, [filter.query, sortedPosts])
 
-  const [selectedSort, setSortAlg] = useState('');
-  const sortPost = (sort)=>{
-    setSortAlg(sort)
-    //т.к. напрямую изменять состояние нельзя
-    setPosts([...posts].sort((a,b)=> a[sort].localeCompare(b[sort])));
-  }
+    const createPost = (newPost) =>{
+        setPosts([...posts, newPost])
+    }
+
+    function removePost(post){
+        setPosts(posts.filter(p=>p.id !== post.id))
+    }
 
     return (
-    <div className="App">
-      <PostCreator create={createPost}/>
-      <MySelect
-        defaultValue={"Сортировка по"}
-        value={selectedSort}
-        onChange={sortPost}
-        options={[
-          {value: 'title', name: 'По названию'},
-          {value: 'body', name: 'По описанию'}
-        ]}
-      />
-      {posts.length !== 0
-          ?
-          <PostList remove={removePost} posts={posts} title="Список постов 1"/>
-          :
-          <h1 style={{textAlign: 'center'}}>Посты не найдены</h1>
-      }
-    </div>
-  );
+        <div className="App">
+            <PostCreator create={createPost}/>
+            <PostFilter
+                filter={filter}
+                setFilter={setFilter}
+            />
+            <PostList remove={removePost} posts={sortedAndSearchPosts} title="Список постов 1"/>
+        </div>
+    );
 }
 
 export default App;

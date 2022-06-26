e97113ab092a8d05a1cb6f32ad9757ceb2eb327f
emil <ruislan.kolomoychenko@gmail.com>
2022-06-26 12:48:41 +0300

    Add dynamic post loading using axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './style/app.css'
 import PostList from "./components/PostList";
 import PostCreator from "./components/PostCreator";
@@ -6,40 +6,48 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import PostService from "./API/PostService";
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'Map', body: "Description"},
-        {id: 2, title: 'In object', body: "Description"},
-        {id: 3, title: 'post', body: "Description"}
-    ]);
-    const [filter, setFilter] = useState({sort:'', query:''});
-    const [modal, setModal] = useState(false);
-    const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
+	const [posts, setPosts] = useState([]);
+	const [filter, setFilter] = useState({sort:'', query:''});
+	const [modal, setModal] = useState(false);
+	const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query);
 
-    const createPost = (newPost) =>{
-        setPosts([...posts, newPost])
-    }
+	//Второй параметр - отслеживаемые компоненты
+	//Возвращаемое значение - при unmount событии
+	useEffect(()=>{
+			fetchPosts().catch();
+		}, []);
 
-    function removePost(post){
-        setPosts(posts.filter(p=>p.id !== post.id))
-    }
+	const createPost = (newPost) =>{
+		setPosts([...posts, newPost])
+	}
 
-    return (
-        <div className="App">
-            <MyButton onClick={()=>setModal(true)}>
-                Создать пост
-            </MyButton>
-            <MyModal visible={modal} setVisible={setModal}>
-                <PostCreator create={createPost}/>
-            </MyModal>
-            <PostFilter
-                filter={filter}
-                setFilter={setFilter}
-            />
-            <PostList remove={removePost} posts={sortedAndSearchPosts} title="Список постов 1"/>
-        </div>
-    );
+	function removePost(post){
+		setPosts(posts.filter(p=>p.id !== post.id))
+	}
+
+	async function fetchPosts(){
+		const posts = await PostService.getAll();
+		setPosts(posts);
+	}
+
+	return (
+		<div className="App">
+			<MyButton onClick={()=>setModal(true)}>
+				Создать пост
+			</MyButton>
+			<MyModal visible={modal} setVisible={setModal}>
+				<PostCreator create={createPost}/>
+			</MyModal>
+			<PostFilter
+				filter={filter}
+				setFilter={setFilter}
+			/>
+			<PostList remove={removePost} posts={sortedAndSearchPosts} title="Список постов 1"/>
+		</div>
+	);
 }
 
 export default App;

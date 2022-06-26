4de50140251837088127965b8b539e506a02ea5b
emil <ruislan.kolomoychenko@gmail.com>
2022-06-26 11:00:35 +0300

    Create smooth animation of adding and deleting posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import Post from "./Post";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const PostList = ({posts, title, remove}) => {
 
-    if(!posts.length){
-        return <h1 style={{textAlign: 'center'}}>Посты не найдены</h1>
-    }
+	if(!posts.length){
+		return <h1 style={{textAlign: 'center'}}>Посты не найдены</h1>
+	}
 
-    return (
-        <div>
-            <h1 style={{textAlign: 'center'}}>{title}</h1>
-            {posts.map(post =>
-                <Post remove={remove} post={post} key={post.id}/>
-            )}
-        </div>
-    );
+	return (
+		<div>
+			<h1 style={{textAlign: 'center'}}>{title}</h1>
+			<TransitionGroup>
+				{posts.map(post =>
+					<CSSTransition
+						key={post.id}
+						timeout={500}
+						classNames="post"
+					>
+						<Post remove={remove} post={post} />
+					</CSSTransition>
+				)}
+			</TransitionGroup>
+		</div>
+	);
 };
 
 export default PostList;
